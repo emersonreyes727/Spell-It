8f6d103c1418989e07edd12e192d821effeac823
emerson reyes <emersonreyes727@gmail.com>
2017-05-16 12:10:54 -0700

    Download cat, tiger, ring, island, gorilla assets

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class GameManager : MonoBehaviour {
+	[SerializeField] private GameObject player;
+
+	public static GameManager instance = null;
+
+	//
+	private bool gameOver = false;
+	private bool gameStart = false;
+	private bool hasKey = false;
+	private bool hasFood = false;
+	private bool hasMed = false;
+
+	//
+	public GameObject Player {
+		get { return player; }
+	}
+
+	public bool GameOver {
+		get { return gameOver; }
+	}
+
+	public bool GameStart {
+		get { return gameStart; }
+	}
+
+	public bool HasKey {
+		get { return hasKey; }
+	}
+
+	public bool HasFood {
+		get { return hasFood; }
+	}
+
+	public bool HasMed {
+		get { return hasMed; }
+	}
+
+	//
+	void Awake () {
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy (gameObject);
+		}
+
+		DontDestroyOnLoad (gameObject);
+
+		Assert.IsNotNull (player);
+	}
+
+	//
+	public void GameIsOver () {
+		gameOver = true;
+	}
+
+	public void GameHasStarted () {
+		gameStart = true;
+	}
+
+	public void PlayerHasKey () {
+		hasKey = true;
+	}
+
+	public void PlayerHasFood () {
+		hasFood = true;
+	}
+
+	public void PlayerHasMed () {
+		hasMed = true;
+	}
+}
---
+++ Assets/Scripts/Notes.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour {
+	// you got the cat, tiger and horse free assets in the asset store wish list
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
