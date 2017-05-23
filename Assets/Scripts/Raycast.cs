4831a7ffcd4d2920f72f6ef5d43b574d5b27f7b3
emerson reyes <emersonreyes727@gmail.com>
2017-05-22 23:11:24 -0700

    Create ball prefab, and raycast script

---
+++ Assets/Scripts/Raycast.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour {
+	private float distance = 10f;
+	private RaycastHit hit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// 
+	void FixedUpdate () {
+		Vector3 forward = transform.TransformDirection (Vector3.forward) * distance;
+		Debug.DrawRay (transform.position, forward, Color.red);
+
+		if (Physics.Raycast (transform.position, forward, out hit)) {
+			if (Input.GetMouseButtonDown(0)) {
+				ShootBall ();
+			}	
+		}
+	}
+
+	void ShootBall () {
+		Debug.Log ("shoot");
+	}
+}
