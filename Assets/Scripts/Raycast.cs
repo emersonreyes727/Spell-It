911b566510cc8a93e4d0bb412f9f25cead13c1c3
emerson reyes <emersonreyes727@gmail.com>
2017-05-22 23:16:48 -0700

    Add ball reset position and instantiate code

--- Assets/Scripts/Raycast.cs
+++ Assets/Scripts/Raycast.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Raycast : MonoBehaviour {
+	[SerializeField] private GameObject ball;
+	[SerializeField] private Transform ballResetPosition;
+
 	private float distance = 10f;
 	private RaycastHit hit;
 
@@ -24,6 +27,7 @@ public class Raycast : MonoBehaviour {
 	}
 
 	void ShootBall () {
+		Instantiate (ball, transform.position, transform.rotation, ballResetPosition);
 		Debug.Log ("shoot");
 	}
 }
