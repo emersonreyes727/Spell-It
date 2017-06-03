e7fb1cc888b59eecd1466c0212f57072d81f9be7
emerson reyes <emersonreyes727@gmail.com>
2017-06-02 17:15:57 -0700

    Add waypoints and welcome island

---
+++ Assets/Scripts/EnterButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterButton : MonoBehaviour {
+	[SerializeField] private Transform player;
+	[SerializeField] private Transform firstWaypoint;
+
+	public void EnterGame () {
+		player.position = firstWaypoint.position;
+
+		GameManager.instance.GameHasStarted ();
+	}
+}
---
+++ Assets/Scripts/WaypointEnable.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointEnable : MonoBehaviour {
+	[SerializeField] private GameObject waypoint1;
+	[SerializeField] private GameObject waypoint2;
+	[SerializeField] private GameObject waypoint3;
+	[SerializeField] private GameObject waypoint4;
+	[SerializeField] private GameObject waypoint5;
+	[SerializeField] private GameObject waypoint6;
+
+	// 
+	void Start () {
+		waypoint1.SetActive (false);
+		waypoint2.SetActive (false);
+		waypoint3.SetActive (false);
+		waypoint4.SetActive (false);
+		waypoint5.SetActive (false);
+		waypoint6.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GameManager.instance.GameStart) {
+			waypoint1.SetActive (true);	
+		}
+
+		if (GameManager.instance.HitC && GameManager.instance.HitA && GameManager.instance.HitT) {
+			waypoint2.SetActive (true);
+			waypoint3.SetActive (true);
+		}
+
+		if (GameManager.instance.HitT2 && GameManager.instance.HitI && GameManager.instance.HitG &&
+			GameManager.instance.HitE && GameManager.instance.HitR) {
+			waypoint4.SetActive (true);
+			waypoint5.SetActive (true);
+			waypoint6.SetActive (true);
+		}
+	}
+}
